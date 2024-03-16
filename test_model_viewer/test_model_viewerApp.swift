46c7097a2b7fd7685dee9a8022060384bb1ec8e7
Hasan M. Hallak <hasanm.hallak@gmail.com>
2024-03-16 02:24:45 +0200

    Initial Commit

---
+++ test_model_viewer/test_model_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_model_viewerApp.swift
+//  test_model_viewer
+//
+//  Created by Hasan M. Hallak on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_model_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
